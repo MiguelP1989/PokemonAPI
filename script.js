3b0ef1e11f799334c1b8e42f1c0d1498c1f24dc7
Miguel Patrocinio <jose.m.patrocinio@hotmail.com>
2020-01-16 17:10:37 +0100

    cardsVisable/animation

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
+const pokedex = document.getElementById("pokedex");
+
+console.log(pokedex);
+
 const fetchPokemon = () => {
   console.log("fetiching pokemon");
 
+  const promises = [];
   for (let i = 1; i <= 150; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    fetch(url)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        console.log("data", data);
-        const pokemon = {
-          name: data.name,
-          id: data.id,
-          image: data.sprites["front_default"],
-          type: data.types.map(type => type.type.name).join(", ")
-        };
-        console.log("pokeon", pokemon);
-      });
+    promises.push(fetch(url).then(res => res.json()));
   }
+
+  Promise.all(promises).then(results => {
+    const pokemon = results.map(data => ({
+      name: data.name,
+      id: data.id,
+      image: data.sprites["front_default"],
+      type: data.types.map(type => type.type.name).join(", ")
+    }));
+    // console.log(pokemon);
+    displayPokemon(pokemon);
+  });
+};
+
+const displayPokemon = pokemon => {
+  console.log(pokemon);
+  const pokemonHTMLString = pokemon
+    .map(
+      pokemon => `
+      <div class="container">
+      <li class="card">
+      <img class="card-image" src="${pokemon.image}"/>
+      <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+      <p class="card-subtitle">Type: ${pokemon.type}</p>
+      </li>
+      </div>`
+    )
+    .join("");
+  // console.log(pokemonHTMLString);
+  pokedex.innerHTML = pokemonHTMLString;
 };
 
 fetchPokemon();
